567d40ec4de09076e9c93ed1bcd841366024f744
ZJian_Note <245482761@qq.com>
2019-04-27 23:21:08 +0800

    选中已授权的权限

--- app/controller/admin/role.js
+++ app/controller/admin/role.js
@@ -58,6 +58,21 @@ class RoleController extends BaseController {
       },
     ]);
     const role_id = this.ctx.request.query._id;
+    const authInfo = await this.ctx.model.RoleAccess.find({ role_id });
+    const access_id_arry = [];
+    authInfo.forEach(function(item) {
+      access_id_arry.push(item.access_id.toString());
+    });
+    accessList.forEach(function(item) {
+      if (access_id_arry.indexOf(item._id.toString()) >= 0) {
+        item.checked = 'true';
+      }
+      item.items.forEach(function(initem) {
+        if (access_id_arry.indexOf(initem._id.toString()) >= 0) {
+          initem.checked = 'true';
+        }
+      });
+    });
     await this.ctx.render('/admin/role/auth', {
       accessList,
       role_id,
@@ -67,7 +82,6 @@ class RoleController extends BaseController {
   async doAuth() {
     const authInfo = this.ctx.request.body;
     const role_id = authInfo.role_id;
-    console.log(role_id);
     await this.ctx.model.RoleAccess.deleteMany({ role_id });
     const access_node = authInfo.access_node;
     for (let i = 0; i < access_node.length; i++) {
